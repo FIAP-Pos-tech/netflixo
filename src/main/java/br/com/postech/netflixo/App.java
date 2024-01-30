a3e07a1c6f28a8cb4fa58b82062b3bb0adf2f8b9
Hakamad4 <erik.hakamada@hotmail.com>
2024-01-29 21:34:58 -0300

    creating the crud

--- src/main/java/br/com/postech/netflixo/App.java
+++ src/main/java/br/com/postech/netflixo/App.java
@@ -1,13 +1,24 @@
 package br.com.postech.netflixo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+import java.util.Properties;
+
+
 @SpringBootApplication
-@EnableReactiveMongoRepositories
+@EnableReactiveMongoRepositories(basePackages = "br.com.postech.netflixo.domain.repository")
 public class App {
+
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication app = new SpringApplication(App.class);
+		Properties prop = new Properties();
+		prop.setProperty("spring.servlet.multipart.location", System.getProperty("java.io.tmpdir"));
+		app.setDefaultProperties(prop);
+		app.run(args);
 	}
+
 }
