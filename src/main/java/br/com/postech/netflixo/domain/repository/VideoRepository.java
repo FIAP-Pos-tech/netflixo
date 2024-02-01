9b58561c0cadfa109d3679b9e56b5a566470c194
Enrico <55825477+maenrico@users.noreply.github.com>
2024-02-01 20:43:34 -0300

    ajustes

--- src/main/java/br/com/postech/netflixo/domain/repository/VideoRepository.java
+++ src/main/java/br/com/postech/netflixo/domain/repository/VideoRepository.java
@@ -2,6 +2,7 @@ package br.com.postech.netflixo.domain.repository;
 
 import br.com.postech.netflixo.domain.entity.Video;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import reactor.core.publisher.Flux;
 @Repository
 public interface VideoRepository extends ReactiveMongoRepository<Video, String> {
     Flux<Video> findByTitle(String title);
+
+    Flux<Video> findByCategory(String category, PageRequest of);
 }
